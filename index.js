9a59a96129d5a83d1ae10b1709d4846dad8bcb78
doreen chepkoech <doreenjudoto@gmail.com>
2024-06-22 17:22:00 +0300

    arithmetic.lab

---
+++ index.js
@@ -0,0 +1,52 @@
+function add() {
+    // we'll fill this in shortly
+  }
+  function add() {
+    return "something";
+  }
+  function add(a, b) {
+    return "something";
+  }
+  function add(a, b) {
+    return a + b;
+  }
+  function add(a, b) {
+    return 1078;
+  }
+  function add(a, b) {
+    return a + b;
+    add ++;
+}
+
+function subtract(a, b) {
+    return a - b;
+    subtract --;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return n + 1;
+}
+
+function decrement(n) {
+    return n - 1;
+}
+
+function makeInt(n) {
+    return parseInt(n);
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
